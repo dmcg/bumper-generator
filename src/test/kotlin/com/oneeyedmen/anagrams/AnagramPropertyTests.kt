dead334dea4494e69f8ca3448c506ee8c217d34d
Duncan McGregor <duncan@oneeyedmen.com>
2023-01-23 14:19:52 +0000

    Filled out AnagramPropertyTests

--- src/test/kotlin/com/oneeyedmen/anagrams/AnagramPropertyTests.kt
+++ src/test/kotlin/com/oneeyedmen/anagrams/AnagramPropertyTests.kt
@@ -1,18 +1,84 @@
 package com.oneeyedmen.anagrams
 
+import org.junit.jupiter.api.Assumptions.assumeTrue
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AnagramPropertyTests {
 
-    @ParameterizedTest
-    @ValueSource(strings = arrayOf( "racecar", "radar", "able was I" ))
-    fun `has the same number of letters as the input`(input: String) {
-        val anagrams = words.anagramsFor(input)
+    @ParameterizedTest(name = "{index} {0}")
+    @MethodSource("inputs")
+    fun `has the same number of letters as the input`(input: String, anagrams: List<String>) {
+        val withoutSpaces = input.withoutSpaces()
         anagrams.forEach { anagram ->
-            assertEquals(input.withoutSpaces().length, anagram.withoutSpaces().length, anagram)
+            assertEquals(
+                withoutSpaces.length,
+                anagram.withoutSpaces().length,
+                "$anagram has the wrong number of letters"
+            )
         }
+    }
+
+    @ParameterizedTest(name = "{index} {0}")
+    @MethodSource("inputs")
+    fun `all the words are in the dictionary`(input: String, anagrams: List<String>) {
+        val wordsAsSet = words.toSet()
+        anagrams.forEach { anagram ->
+            anagram.split(' ').forEach { word ->
+                assertTrue(
+                    wordsAsSet.contains(word),
+                    "$word from $anagram is not in dictionary"
+                )
+            }
+        }
+    }
+
+    @ParameterizedTest(name = "{index} {0}")
+    @MethodSource("inputs")
+    fun `input and anagrams have the same letters`(input: String, anagrams: List<String>) {
+        val inputLetters = input.withoutSpaces().uppercase().toCharArray().sorted()
+        anagrams.forEach { anagram ->
+            assertEquals(
+                inputLetters,
+                anagram.withoutSpaces().toCharArray().sorted(),
+                "$anagram does not have the right letters"
+            )
+        }
+    }
+
+    @ParameterizedTest(name = "{index} {0}")
+    @MethodSource("inputs")
+    fun `one anagram contains input words`(input: String, anagrams: List<String>) {
+        assumeTrue(input.isNotBlank())
+        val inputSorted = input.uppercase().split(' ').sorted()
+        assertEquals(
+            1,
+            anagrams.filter { it.split(' ').sorted() == inputSorted }.size
+        )
+    }
+
+    fun inputs() = inputs
+
+    private val pathologicals = listOf("a", "")
+    private val upTothreeLetterWords = words.filter { it.length < 4 }
+    private val shortWords = words.filter { it.length < 6 }
+    private val randomShortWords = (1..30).map { shortWords.random() }
+    private val randomWordPairs = (1..30).map {
+        shortWords.random() + " " +
+                shortWords.random()
+    }
+    private val randomWordTriples = (1..30).map {
+        upTothreeLetterWords.random() + " " +
+                shortWords.random() + " " +
+                upTothreeLetterWords.random()
+    }
+    private val inputs = (pathologicals + randomShortWords + randomWordPairs + randomWordTriples).map {
+        arguments(it, words.anagramsFor(it))
     }
 }
 
