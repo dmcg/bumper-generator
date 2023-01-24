1ce5532da7458d2c588d2ac88e95f699270b8472
Duncan McGregor <duncan@oneeyedmen.com>
2023-01-24 14:11:38 +0000

    More speed tests

--- src/test/kotlin/com/oneeyedmen/anagrams/AnagramSpeedTests.kt
+++ src/test/kotlin/com/oneeyedmen/anagrams/AnagramSpeedTests.kt
@@ -1,15 +1,52 @@
 package com.oneeyedmen.anagrams
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty
 import kotlin.math.sqrt
 import kotlin.system.measureTimeMillis
 
-
+@EnabledIfSystemProperty(named = "run-slow-tests", matches = "true")
 class AnagramSpeedTests {
 
     @Test
     fun `anagrams for REFACTORING TO`() {
         val input = "REFACTORING TO"
+        val repetitions = 5
+        val meanAndDeviation = (1..repetitions).map {
+            measureTimeMillis {
+                words.anagramsFor(input)
+            }.toDouble()
+        }.culledMeanAndDeviation()
+        println("Duration ${meanAndDeviation.first.toLong()} ± ${meanAndDeviation.second.toLong()}")
+    }
+
+    @Test
+    fun `anagrams for REFACTORING TO K`() {
+        val input = "REFACTORING TO K"
+        val repetitions = 5
+        val meanAndDeviation = (1..repetitions).map {
+            measureTimeMillis {
+                words.anagramsFor(input)
+            }.toDouble()
+        }.culledMeanAndDeviation()
+        println("Duration ${meanAndDeviation.first.toLong()} ± ${meanAndDeviation.second.toLong()}")
+    }
+
+    @Test
+    fun `anagrams for REFACTORING TO KO`() {
+        val input = "REFACTORING TO KO"
+        val repetitions = 5
+        val meanAndDeviation = (1..repetitions).map {
+            measureTimeMillis {
+                words.anagramsFor(input)
+            }.toDouble()
+        }.culledMeanAndDeviation()
+        println("Duration ${meanAndDeviation.first.toLong()} ± ${meanAndDeviation.second.toLong()}")
+    }
+
+    @Test
+    fun `anagrams for REFACTORING TO KOT`() {
+        val input = "REFACTORING TO KOT"
         val repetitions = 5
         val meanAndDeviation = (1..repetitions).map {
             measureTimeMillis {
