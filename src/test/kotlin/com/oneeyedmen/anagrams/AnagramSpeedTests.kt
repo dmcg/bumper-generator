fe400f688c733b95ad03cf51db88ccb49e730277
Duncan McGregor <duncan@oneeyedmen.com>
2023-01-30 13:28:20 +0000

    Improve AnagramSpeedTests

--- src/test/kotlin/com/oneeyedmen/anagrams/AnagramSpeedTests.kt
+++ src/test/kotlin/com/oneeyedmen/anagrams/AnagramSpeedTests.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 import kotlin.math.sqrt
 import kotlin.system.measureTimeMillis
+import kotlin.test.assertEquals
 
 
 @TestMethodOrder(MethodOrderer.MethodName::class)
@@ -12,22 +13,31 @@ class AnagramSpeedTests {
 
     @Test
     fun `anagrams for REFACTORING`() {
-        report("REFACTORING", 50)
+        report("REFACTORING", repetitions = 50, expectedResultCount = 9017)
     }
 
     @Test
     fun `anagrams for REFACTORING TO`() {
-        report("REFACTORING TO", 10)
+        report("REFACTORING TO", repetitions = 10, expectedResultCount = 128270)
     }
 }
 
-private fun report(input: String, repetitions: Int) {
-    val meanAndDeviation = (1..repetitions).map {
-        measureTimeMillis {
-            words.anagramsFor(input)
-        }.toDouble()
-    }.culledMeanAndDeviation()
-    println("Duration ${meanAndDeviation.first.toLong()} ± ${meanAndDeviation.second.toLong()}")
+private fun report(input: String, repetitions: Int, expectedResultCount: Int) {
+    val timeAndResultCounts = (1..repetitions).map {
+        val resultCount: Int
+        val timeMs = measureTimeMillis {
+            resultCount = words.anagramsFor(input).size
+        }
+        resultCount to timeMs
+    }
+    val meanAndDeviation = timeAndResultCounts
+        .map { it.second.toDouble() }
+        .culledMeanAndDeviation()
+    println(
+        "Duration ${meanAndDeviation.first.toLong()} ± ${meanAndDeviation.second.toLong()}"
+    )
+
+    timeAndResultCounts.forEach { (count, _) -> assertEquals(expectedResultCount, count) }
 }
 
 private fun List<Double>.culledMeanAndDeviation(): Pair<Double, Double> = when {
