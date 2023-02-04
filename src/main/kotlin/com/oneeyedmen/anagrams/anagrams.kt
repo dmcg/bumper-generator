923872224569887759ebfcbcad4c0222c53956c7
Duncan McGregor <duncan@oneeyedmen.com>
2023-02-04 12:53:35 +0000

    Revert "minusLettersIn returns WordInfo"
    This reverts commit 5a636fd7003901f1654733c7399d59e290b7083c.

--- src/main/kotlin/com/oneeyedmen/anagrams/anagrams.kt
+++ src/main/kotlin/com/oneeyedmen/anagrams/anagrams.kt
@@ -43,10 +43,11 @@ private fun process(
         instrumentation(MinusLettersInInvocation(input, wordInfo))
         val remainingLetters = input.minusLettersIn(wordInfo)
         when {
-            remainingLetters.letterBitSet == 0 ->
+            remainingLetters.isEmpty() ->
                 collector(prefix + wordInfo)
+
             depth > 1 -> process(
-                input = remainingLetters,
+                input = WordInfo(remainingLetters),
                 words = remainingCandidateWords,
                 collector = collector,
                 prefix = prefix + wordInfo,
@@ -77,26 +78,8 @@ internal class WordInfo(
         !letterBitSet.hasLettersNotIn(input.letterBitSet) &&
                 this.word.couldBeMadeFromTheLettersIn(input.word)
 
-    fun minusLettersIn(other: WordInfo): WordInfo {
-        val remainingLetters = this.word.toCharArray()
-        other.word.forEach { char ->
-            val index = remainingLetters.indexOf(char)
-            if (index == -1)
-                error("BAD")
-            remainingLetters[index] = '*'
-        }
-        val result = CharArray(this.word.length - other.word.length)
-        var resultLetterBitSet = 0
-
-        var index = 0
-        remainingLetters.forEach { char ->
-            if (char != '*') {
-                result[index++] = char
-                resultLetterBitSet = resultLetterBitSet or (1 shl char - 'A')
-            }
-        }
-        return WordInfo(listOf(String(result)), resultLetterBitSet)
-    }
+    fun minusLettersIn(other: WordInfo): String =
+        this.word.minusLettersIn(other.word)
 }
 
 internal fun Int.hasLettersNotIn(other: Int) = (this and other) != this
@@ -121,6 +104,24 @@ internal fun String.couldBeMadeFromTheLettersIn(letters: String): Boolean {
     }
     return true
 }
+
+private fun String.minusLettersIn(word: String): String {
+    val remainingLetters = this.toCharArray()
+    word.forEach { char ->
+        val index = remainingLetters.indexOf(char)
+        if (index == -1)
+            error("BAD")
+        remainingLetters[index] = '*'
+    }
+    val result = CharArray(this.length - word.length)
+    var index = 0
+    remainingLetters.forEach { char ->
+        if (char != '*')
+            result[index++] = char
+    }
+    return String(result)
+}
+
 
 private fun List<WordInfo>.combinations(): Set<String> =
     mutableListOf<String>().apply { permuteInto(this) }
